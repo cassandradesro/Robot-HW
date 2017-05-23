6362d39fd0945075057afebc209293dfbd8535dd
cassandradesro <cdesro10@gmail.com>
2017-05-23 13:49:56 -0400

    Start

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+console.log("Hello World from main.js!");
+
+
+
+var doTheThing = function() {
+	console.log("do the thing!");
+
+	// This function runs when the user presses the "Go!" button.
+
+	// It needs to read what's been entered in the three input elements (look it up)
+	// and apply those values to the styling of the various elements that comprise the robot.
+
+}
